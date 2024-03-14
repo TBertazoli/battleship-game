720161e52bb1db63d5cd7c8f2003665ba9525682
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-03-14 13:45:01 -0500

    created pseudocode for the game

---
+++ script.js
@@ -0,0 +1,11 @@
+// Pseudocode
+// 1. Init function will Defines
+//1.1 boat sizes and number of boats(1 * 2, 2 * 3, 1 * 4, 1 * 5)
+//1.2 Create a function that draws a grid (2 grids for player and computer) - 2 * 2x2 grid
+
+// 2. Create a function that randomly places the ships on the grid. (If time permit create a function that allows the user to place the ships on the grid)
+// 3. Create a function to randomly choose who plays first
+// 4. Create a function that allows the user to click on a cell and fire a shot.
+// 5. Create a function that allows the computer to fire a shot.
+// 6. Create a function that checks if cell is a hit or a miss.
+// 7. Create a function that checks for number of hits, whoever has all the hits completed wins the game.
